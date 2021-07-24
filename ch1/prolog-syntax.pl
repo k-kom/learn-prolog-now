ed1e7bbd066ee9c81a84e182d297afd7a9c6af77
k-kom <hal02660@gmail.com>
2021-07-24 22:05:02 +0900

    end of ch1.2

---
+++ ch1/prolog-syntax.pl
@@ -0,0 +1,6 @@
+%% facts, rules and queries built out of _terms_.
+%% terms: atoms, numbers, variables, and complex terms(structures)
+
+%% atom: butch, 'Vincent', :-, ;
+
+%% variables: X, Y, _tag (_ is anonymous variable)
