6a98287ca4605f57dea4a7b5f8abc94ab076c052
k-kom <hal02660@gmail.com>
2021-07-24 21:22:10 +0900

    end of ch1.1

--- ch1/some-simple-examples.pl
+++ ch1/some-simple-examples.pl
@@ -2,25 +2,55 @@
 woman(mia).
 woman(jody).
 woman(yolanda).
-playsAirGuitar(jody).
+%% playsAirGuitar(jody).
 party.
 
 
 %% KB2
 happy(yolanda).
 listen2Music(mia).
-%% :- means <-
+%% :- means <- (implication)
 listen2Music(yolanda):- happy(yolanda).
 playsAirGuitar(mia):- listen2Music(mia).
 playsAirGuitar(yolanda):- listen2Music(yolanda).
 
 %% KB3
-happy(vincent).
-listen2Music(butch).
-playsAirGuitar(vincent):-
-    listen2Music(vincent),
-    happy(vincent).
-playsAirGuitar(butch):-
-    happy(butch).
-playsAirGuitar(butch):-
-    listen2Music(butch).
+%% happy(vincent).
+%% listen2Music(butch).
+%% playsAirGuitar(vincent):-
+%%     listen2Music(vincent),
+%%     happy(vincent).
+%% logical disjunction (or)
+%% playsAirGuitar(butch):-
+%%     happy(butch).
+%% playsAirGuitar(butch):-
+%%     listen2Music(butch).
+
+%% also expressing disjunction
+%% playsAirGuitar(butch):-
+%%     happy(butch);
+%%     listen2Music(butch).
+
+%% KB4
+%% woman(mia).
+%% woman(jody).
+%% woman(yolanda).
+
+loves(vincent,mia).
+loves(marsellus,mia).
+loves(pumpkin,honey_bunny).
+loves(honey_bunny,pumpkin).
+
+%% KB5
+%% loves(vincent,mia).
+%% loves(marsellus,mia).
+%% loves(pumpkin,honey_bunny).
+%% loves(honey_bunny,pumpkin).
+jealous(X,Y):- loves(X,Z), loves(Y,Z), X\=Y.
+
+%% ?- jealous(X,Y).
+%% X = vincent,
+%% Y = marsellus ;
+%% X = marsellus,
+%% Y = vincent ;
+%% false.
