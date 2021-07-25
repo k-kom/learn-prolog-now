5e9d67aa2977d457eb67320586a3a39f182a41e8
k-kom <hal02660@gmail.com>
2021-07-25 15:28:22 +0900

    wip ch2

---
+++ ch2/unification.pl
@@ -0,0 +1,11 @@
+%% KB4 from chapter 1
+%% woman(mia).
+%% woman(jody).
+%% woman(yolanda).
+
+%% loves(vincent,mia).
+%% loves(marsellus,mia).
+%% loves(pumpkin,honey_bunny).
+%% loves(honey_bunny,pumpkin).
+
+%% terms can be 1. constants (atoms or numbers), 2. variables, 3. complex terms
